120b6e658da4f91569a84fbe89c77d0fdc6c8e59
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-13 16:58:33 +0530

    feat: Add Event and Attendee API resource controllers and routes, and enable API tokens for users.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\AttendeeController;
+use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
