85b2e8b4ee970df6ce988a52245104f555dc0979
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-13 16:15:39 +0530

    feat: establish event and attendee models with relationships and core fields.

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendee extends Model
 {
     //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
