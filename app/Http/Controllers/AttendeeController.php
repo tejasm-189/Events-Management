8c060e83bd1bf7bc649022ed21a4f1ddb0c748ed
Tejas M <143952904+tejasm-189@users.noreply.github.com>
2025-12-15 15:12:14 +0530

    feat: Implement event-scoped attendee management with route model binding, API resources, and updated Attendee fillable fields.

--- app/Http/Controllers/AttendeeController.php
+++ app/Http/Controllers/AttendeeController.php
@@ -3,31 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Http\Resources\AttendeeResource;
+use App\Models\Attendee;
 
 class AttendeeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event)
     {
         //
+        $attendees = $event->attendees()->latest();
+        return AttendeeResource::collection($attendees->paginate());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
         //
+        $attendee = $event->attendees()->create(
+            [
+                'user_id' => 1
+            ]
+        );
+        return new AttendeeResource($attendee);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event, Attendee $attendee)
     {
         //
+        return new AttendeeResource($attendee);
     }
 
     /**
